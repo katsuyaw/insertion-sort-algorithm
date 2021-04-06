00e6c7eaebdfaff1cc3e13a9dd8ff1136c91d72f
katsuyaw <kw@kmail.me>
2021-04-06 16:58:48 -0700

    Add comments to insertSort

--- insertion.cpp
+++ insertion.cpp
@@ -8,16 +8,20 @@ void insertionSort(T* array, int size)
     T key;
 
     for(target=0; target<size; target++){
+        // condition: next element of target is smaller than target
         if(array[target]>array[target+1]){
+            // record the location of the next element
             location = target + 1;
+            // store the element to key
             key = array[location];
 
+            // keep moving each element to the right by 1
             do{
                 array[location] = array[location-1];
                 location--;
-            }
+            }// until the element moving is smaller than key
             while(location>0 && array[location-1]>key);
-            
+            // locate key to the position
             array[location] = key;
         }
     }
